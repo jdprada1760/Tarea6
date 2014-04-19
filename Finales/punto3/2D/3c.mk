b07d9b4881f76a09c12c0d34f5fc14cae964a71f
jdprada1760 <jd.prada1760@uniandes.edu.co>
2014-04-19 18:41:02 -0500

    Makefile del punto 3-c

--- Finales/punto3/2D/3c.mk
+++ Finales/punto3/2D/3c.mk
@@ -3,6 +3,9 @@ all: evolve.dat
 evolve.dat: evolve.x data.dat
 	./evolve.x 1000 5 data.dat
 	python plots.py evolve.dat
+	rm -f *.x
+	rm -f *.dat
+	echo Clean-Done
 
 evolve.x: IC.x evolve.c
 	cc evolve.c -o evolve.x -lm
@@ -13,7 +16,5 @@ data.dat: IC.x
 IC.x:	IC.c
 	cc IC.c -o IC.x -lm
 
-clean: 
-	rm -f *.x
-	rm -f *.dat
+clean:
 
