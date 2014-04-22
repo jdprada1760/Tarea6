a36e6882f7b6ed180dd66d9b5293053af20aa180
jdprada1760 <jd.prada1760@uniandes.edu.co>
2014-04-22 00:56:22 -0500

    Grafica los estados de evolucion

---
+++ Finales/bono/plots.py
@@ -0,0 +1,47 @@
+import numpy as np;
+import matplotlib.pyplot as plt
+import sys
+
+# Obtiene los datos
+data = []
+data.append(np.loadtxt("colapso1.data").T)
+
+data.append(np.loadtxt("colapso2.data").T)
+
+data.append(np.loadtxt("colapso3.data").T)
+
+data.append(np.loadtxt("colapso4.data").T)
+
+data.append(np.loadtxt("colapso5.data").T)
+
+energy = np.loadtxt("energy.data");
+
+data = np.array(data)
+
+# Grafica
+
+lim = 100;
+for i in range(len(data)):
+    temp = data[i]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xlabel("X_pc")
+    ax.set_ylabel("Y_pc")
+    ax.set_xlim((-lim, lim))
+    ax.set_ylim((-lim, lim))
+    ax.scatter(temp[0],temp[1], s = 5, alpha = 0.5, c = 'b')
+    
+    # Guarda la grafica
+    filename = "colapso_Gravitacional" + str(j+1)
+    plt.savefig(filename + '.pdf',format = 'pdf', transparent=True)
+    plt.close()
+
+# Grafica las diferencias de energia
+
+energy = abs((energy[0:-2] - energy[1:-1])/energy[0:-2)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlabel("dE/E")
+ax.plot(np.arange(0,len(energy)),energy)
+plt.savefig("Energy_evol" + '.pdf',format = 'pdf', transparent=True)
+plt.close()
